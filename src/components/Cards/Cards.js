601080f537b42efd03720f55091dd1b3cc345b24
jannatnury <jannatnury253@gmail.com>
2022-04-05 23:23:23 +0600

    rating added

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -18,7 +18,7 @@ const Cards = (props) => {
                     <p className="text-gray-700 text-base">{review}</p>
                 </div>
                 <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Ratings:{rating}</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Rating:{rating}</span>
                 </div>
             </div>
         </div>
